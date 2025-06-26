79d64d921db9ef8bd43095aa421f12c02414d59a
llz <2339458297@qq.com>
2025-06-26 18:18:15 +0800

    注释

--- main.cpp
+++ main.cpp
@@ -7,18 +7,19 @@ void sort(int* arr, int size)
     for (int i = 0; i < size - 1; i++)
     {
         for (int j = 0; j < size - i - 1; j++) 
-	{
-	    if (arr[j] > arr[j + 1])
 	    {
-		tmp = arr[j];
-		arr[j] = arr[j+1];
-		arr[j+1] = tmp;
+	        if (arr[j] > arr[j + 1])
+	        {
+		    tmp = arr[j];
+		    arr[j] = arr[j+1];
+		    arr[j+1] = tmp;
+	        }   
 	    }
-	}
     }	    
 }
 
 int main() {
+    /// llz1
     int arr[] = {12,1,2,3,4};
     int size = sizeof(arr)/sizeof(arr[0]);
     sort(arr,size);
